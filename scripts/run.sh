41298047c2a3e74cfe5710d5d1206ece55fdf512
ttt-ishi03 <tai.ishisaki@gmail.com>
2024-07-15 08:46:46 +0900

    fix: url in run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,10 @@
 docker-compose restart
 
-echo "access to your web page"
-ip="`ip a | grep 'inet ' | awk '{print $2}' | cut -d'/' -f1 | grep 10.0`"
-echo "http://$ip:8000/"
+# Get the IP address
+IP_ADDR=$(hostname -I | awk '{print $1}')
+
+# Construct the URL
+URL="http://$IP_ADDR:8000/"
+
+# Echo the URL
+echo "Access the application at: $URL"
