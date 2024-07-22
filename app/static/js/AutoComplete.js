d89aa161e5529e99ca4156914877dbcd2e3bd7f9
TANIGUCHIREI <taniguchirei0j@gmail.com>
2024-07-22 16:02:14 +0900

    add hogehoge

--- app/static/js/AutoComplete.js
+++ app/static/js/AutoComplete.js
@@ -19,7 +19,7 @@ if (document.getElementById('liData').value){
 var form = document.getElementById('searchForm');
 
 const autoCompleteJS = new autoComplete({
-placeHolder: "作者・声優・舞台・ジャンル・題材・属性　など",
+placeHolder: "例:いちごタルト、カレー、ショートケーキ　など",
 data: {
 src: full_categoires_list,
 cache: true,
