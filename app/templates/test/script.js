a41e45c3bf15a6e81264559f8c44c75c98fc3f98
TANIGUCHIREI <taniguchirei0j@gmail.com>
2024-07-25 02:35:33 +0900

    delete pycache and fixed html

--- app/templates/test/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('toggle-switch').addEventListener('change', function() {
-    var content = document.getElementById('toggle-content');
-    if(this.checked) {
-        content.style.display = 'block';
-    } else {
-        content.style.display = 'none';
-    }
-});
